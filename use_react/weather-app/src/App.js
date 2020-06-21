95f8aaaf97f61fbe2dbe94eb36b1e61e522709b3
linh <thuytpham9@gmail.com>
2020-06-20 20:57:43 -0700

    Updated

--- use_react/weather-app/src/App.js
+++ use_react/weather-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Weather from './components/weather';
+
+
 
 
 
@@ -16,3 +17,7 @@ function App() {
 };
 
 export default App;
+
+
+
+
