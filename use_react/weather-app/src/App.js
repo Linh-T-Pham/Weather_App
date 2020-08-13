6df7db629dcbef17125a9735ad116feab759cfd8
linh <thuytpham9@gmail.com>
2020-08-12 18:30:03 -0700

    fetch api

--- use_react/weather-app/src/App.js
+++ use_react/weather-app/src/App.js
@@ -1,22 +1,77 @@
-import React from 'react';
-import './App.css';
-import Weather from './components/weather';
+
+import React, { useState, useEffect } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
 
+// const useStyles = makeStyles((theme) => ({
+//   input: {
+//     flex: 1,
+//     width: 500,
+//     height: 40,
+//     fontSize: 15
+//   },
+//   iconButton: {
+//     padding: 10,
+//   },
+//   weatherContainer: {
+//     fontSize: 40,
+//     paddingLeft: 440,
+//   },
+//   title: {
+//     fontSize: 40,
+//     paddingLeft: 500
+//   },
+//   weatherInfo: {
+//     fontSize: 30,
+//     paddingLeft:500,
+//     paddingTop: 20
+//   }
+// }));
+
+const api = {
+  key: "5fc2cf4502bdb80a9a86054635dcb41c",
+  base: "https://api.openweathermap.org/data/2.5"
+}
+
+export default function Weather() {
+  // const classes = useStyles();
+  const [city, setCity] = useState([]);
+  const [cityData, setCityData] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${api.base}/weather?q=${city}&units=metric&APPID=${api.key}`)
+      const getResult = await res.json();
+      setCityData(getResult);
+      }
+      fetchData();
+    }, []);
 
 
+  const handleClick = (event) => {
+    setCity(event.target.value);
+  }; 
 
-// api call 
-
-function App() {
+  
+  console.log(cityData)
   return (
-    <div className="App">
-      <Weather/>
-    </div>
-  );
-};
+  <div>
+      <h1>A Cool App</h1>
+      <Select value={city} onChange={handleClick}>       
+        <MenuItem value={'San Francisco'}>San Francisco</MenuItem>
+        <MenuItem value={'Daly City'}>Daly City</MenuItem>
+        <MenuItem value={'Hawaii'}>Hawaii</MenuItem>
+      </Select> 
 
-export default App;
+    {city}
+   {/* {cityData.weather[0].main} */}
+  </div>
+  
+  );
+}
+  
 
 
 
--- use_react/weather-app/src/components/weather.jsx
+++ use_react/weather-app/src/components/weather.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
+// import React, { useState } from 'react';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import Select from '@material-ui/core/Select';
 
-export default function Weather() {
-  const [city, setCity] = React.useState(null);
-  // var city = null;
+// export default function Weather() {
+//   const [city, setCity] = React.useState(null);
+//   const [query, setQuery] = useState('');
+//   // var city = null;
 
-  const handleClick = (event) => {
-    setCity(event.target.value);
-    // city = event.target.value
-  };
+//   const handleClick = (event) => {
+//     setCity(event.target.value);
+//     // city = event.target.value
+  
+//   };
 
-  return (
-    <div>
-       <h1>Simple Weather App</h1>
-      <Select value={city} onChange={handleClick}>
-        <MenuItem value={'San Francisco'}>San Francisco</MenuItem>
-        <MenuItem value={'Daly City'}>Daly City</MenuItem>
-        <MenuItem value={'Hawaii'}>Hawaii</MenuItem>
-      </Select>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//        <h1>Simple Weather App</h1>
+//       <Select value={city} onChange={handleClick}>
+//         <MenuItem value={'San Francisco'}>San Francisco</MenuItem>
+//         <MenuItem value={'Daly City'}>Daly City</MenuItem>
+//         <MenuItem value={'Hawaii'}>Hawaii</MenuItem>
+//       </Select>
+//     </div>
+//   );
+// }
   
 
